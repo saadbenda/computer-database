20d4158947f638d56ef18c5727a4b80261d1ecd9
Saad BEN <saad.benda@gmail.com>
2020-07-22 10:04:30 +0200

    Update DashboardController.java

--- webapp/src/main/java/com/excilys/controllers/DashboardController.java
+++ webapp/src/main/java/com/excilys/controllers/DashboardController.java
@@ -1,114 +1,96 @@
-//package controllers;
-//
-//import java.util.Arrays;
-//import java.util.HashMap;
-//import java.util.List;
-//
-//import javax.servlet.http.HttpSession;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.MessageSource;
-//import org.springframework.context.annotation.Scope;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.ModelMap;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.SessionAttributes;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//import mapper.MapperDto;
-//import model.Company;
-//import model.Computer;
-//import persistence.CompanyRepository;
-//import persistence.ComputerRepository;
-//import service.Service;
-//import validation.Validation;
-//
-//@Controller
-//@RequestMapping("dashboard")
-//@SessionAttributes(value = {"id","page","pageSize","order"})
-//
-//public class DashboardController {
-//	@Autowired
-//	Service service;
-//
-//	@Autowired
-//	MapperDto mapperDto;
-//
-//	@Autowired
-//	Validation validation;
-//
-//	@Autowired
-//	CompanyRepository companyRepo;
-//	
-//	@Autowired
-//	ComputerRepository computerRepo;
-//	
-//
-//
-//	
-//	public static final String DASHBOARD = "dashboard";
-//	
-//	
-//	@GetMapping
-//	public String doGet(ModelMap modelMap, HttpSession session) {		
-//		List<Computer> computers;
-//		//Pageable page = PageRequest.of(offset, limit,Direction.ASC,state);
-//		
-//		page = new PageRequest(page, pageSize, Direction.ASC);
-//		computers=computerRepo.findByNameContainingIgnoreCase((String) session.getAttribute("search"), page);
-//		
-//		
-//		//List<Computer> computers = computerRepo.searchByName(session.getAttribute(name));
-//		
-//		
-//		
-//		//List<Computer> computers=computerRepo.findAll();
-//		modelMap.addAttribute("computers",computers);		
-//		return DASHBOARD;
-//	}
-//	
-//	@PostMapping
-//	public String doPost(@ModelAttribute(name="search") String search, @ModelAttribute(name="page") String page, @ModelAttribute(name="result") String result) {
-//				
-//		System.out.println(search);
-//		System.out.println(page);
-//		System.out.println(result);			
-//		return DASHBOARD;
-//		
-//	}
-//	
-//	@PostMapping(value="/delete")
-//	public ModelAndView deleteComputer(@RequestParam(value = "selection") String selection) {
-//
-//		ModelAndView modelAndView = new ModelAndView("redirect:/dashboard");
-//		List<String> computers = Arrays.asList(selection.split(","));
-//		for (String s : computers) {
-//			computerService.deleteComputer(Integer.parseInt(s));
-//		}
-//
-//		return modelAndView;
-//	}
-//	
-//	@PostMapping(value="/order")
-//	public ModelAndView order(@RequestParam(value = "selection") String selection) {
-//
-//		ModelAndView modelAndView = new ModelAndView("redirect:/dashboard");
-//		List<String> options = Arrays.asList(selection.split(","));
-//		for (String s : options) {
-//			computerService.deleteComputer(Integer.parseInt(s));
-//		}
-//
-//		return modelAndView;
-//	}
-//		
-//	
-//	
-//	
-//	
-//}
+package controllers;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import dto.CompanyDto;
+import dto.ComputerDto;
+import exceptions.DateParseException;
+import exceptions.NameEmptyException;
+import mapper.MapperDto;
+import model.Company;
+import model.Computer;
+import persistence.CompanyRepository;
+import persistence.ComputerRepository;
+import service.Service;
+import validation.Validation;
+
+@Controller
+@RequestMapping("editComputer")
+
+@SessionAttributes(value = {"lang", "id"})
+
+
+public class EditComputerController {
+	@Autowired
+	Service service;
+
+	@Autowired
+	MapperDto mapperDto;
+
+	@Autowired
+	Validation validation;
+
+	@Autowired
+	MessageSource messageSource;
+	
+	@Autowired
+	CompanyRepository companyRepo;
+	
+	@Autowired
+	ComputerRepository computerRepo;
+	
+	public static final String EDITCOMPUTER = "editComputer";
+	public static final String DASHBOARD = "dashboard";
+	
+	@GetMapping
+	public String doGet(@RequestParam(required=false, name="id")  String id, ModelMap modelMap, HttpSession session) {
+		
+		if ((boolean) session.getAttribute("id")) {
+			id=(String) session.getAttribute("id");
+		}
+		Computer computer = computerRepo.findById(id);
+		modelMap.addAttribute("computer", computer);
+		List<Company> companies = companyRepo.findAll();
+		modelMap.addAttribute("companies", companies);
+		return EDITCOMPUTER;
+		
+	}
+	
+	@PostMapping
+	public String doPost(@ModelAttribute(name="Computer") ComputerDto computerDto, CompanyDto companyDto, ModelMap modelMap) throws DateParseException, NameEmptyException {
+		
+		/*computerDto.setCompany(companyDto);
+		Computer computer = mapperDto.fromComputerDtoToComputer(computerDto);
+		//computerRepo.update(computer);
+		*/
+		System.out.println("------------------------" +computerDto);
+		System.out.println("------------------------" +companyDto);
+		
+		//computerDto.setCompany(companyDto);
+		System.out.println(computerDto.toString());
+		
+		return DASHBOARD;
+		
+	}
+	
+}
